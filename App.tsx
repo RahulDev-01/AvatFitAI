4ab46aa5b71308e5a2775b75b5fc007f21d93c44
S.Rahul <s.rahul5116@outlook.com>
2026-03-15 10:14:29 +0530

    App.tsx Updated

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 import "./global.css";
-import { registerRootComponent } from 'expo';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
 export default function App() {
   return <RootNavigator />;
 }
-
-registerRootComponent(App);
